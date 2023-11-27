b24d3b73fb24bf21d2aa5dc36ef3747e288e73cb
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-11-27 19:17:52 +0100

    Fixed

--- index.js
+++ index.js
@@ -7,35 +7,52 @@ if(userName === null || userName.trim() === '') {
 
 alert(`Let's do numbers`);
 
-const firstNumber = +prompt('Enter first number').trim();
-const secondNumber = firstNumber !== null && +prompt('Enter second number').trim();
-
-if (isNaN(firstNumber)) {
-    alert(`First value not a number`);
-} else if (isNaN(secondNumber) || secondNumber === null) {
-    alert(`Second value not a number`);
+const firstNumber = prompt('Enter first number');
+console.log(firstNumber)
+if(firstNumber === null){
+    alert('Refresh and try again')
 } else {
-    alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
-    alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
-    alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
-    alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+    const secondNumber = prompt('Enter second number');
+    if (isNaN(firstNumber)) {
+        alert(`First value not a number`);
+    } else if (isNaN(secondNumber)) {
+        alert(`Second value not a number`);
+    } else if (secondNumber === null) {
+        alert('Refresh and try again')
+    } else {
+        alert(`${firstNumber} + ${secondNumber} = ${+firstNumber + +secondNumber}`);
+        alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+        alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+        alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+    }
 }
 
+
 alert(`Let's do equal`)
-const firstValue = prompt('Enter first value').trim()
-const secondValue = prompt('Enter second value').trim()
-if(firstValue !== null || secondValue !== null){
-    alert(firstValue === secondValue)
+const firstValue = prompt('Enter first value')
+if(firstValue === null){
+    alert('Refresh and try again')
+} else if(firstValue.trim() === '') {
+    alert('Enter something')
 } else {
-    alert('You should type something')
+    const secondValue = prompt('Enter second value')
+    if(secondValue === null){
+        alert('Refresh and try again')
+    } else if(secondValue.trim() === '') {
+        alert('Enter something')
+    } else {
+        alert(firstValue.trim() === secondValue.trim())
+    }
 }
 
 alert(`Let's do another numbers`)
-let numbers = +prompt('Enter five numbers').trim()
-if(isNaN(numbers) || numbers === null){
+let numbers = prompt('Enter five numbers')
+if(isNaN(numbers)){
     alert(`Your value not a number`)
-} else if(numbers.toString().length !== 5){
-    alert(`Please enter 5 digits`)
+} else if(numbers === null || numbers.trim() === ''){
+    alert(`Your value not a number`)
+} else if((!numbers.includes('-') && numbers.toString().length !== 5) || (numbers.includes('-') && numbers.toString().length !== 6)){
+    alert(`Please enter 5 digits or '-' and 5 digits`)
 } else {
     const number5 = numbers % 10
     numbers = (numbers - number5) / 10
